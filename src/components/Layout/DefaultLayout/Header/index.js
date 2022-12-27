21985d8dc2e5f96c71eacfc20626f4e3f698dc8a
ttHien67 <tthanhhien2k1@gmail.com>
2022-12-27 14:31:20 +0700

    Xây dựng UI phần Header #3

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import images from '@/assets/images';
 import Tippy from '@tippyjs/react/headless';
 import AccountItem from '@/components/AccountItem';
+import Button from '@/components/Button';
 
 import { Wrapper as PopperWrapper } from '@/components/Popper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -51,7 +52,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </div>
     );
